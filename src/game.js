875d29d3c0df448a0008552872c6ec2083bfa0cf
Gugi Gumilang <gugi.gumilang@gmail.com>
2019-05-13 22:45:24 +0200

    2nd commit

--- src/game.js
+++ src/game.js
@@ -1,7 +1,66 @@
 import React from "react";
+import Choice from "./choice";
+import Ending from "./ending";
+import adventures from "./adventures";
 
 export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            current_adventure: 0,
+            adventures: adventures
+        };
+        this.changeAdventure = this.changeAdventure.bind(this);
+    }
+
+    changeAdventure(to) {
+        this.setState({
+            current_adventure: to
+        });
+    }
+    resetGame() {
+        this.setState({
+            current_adventure: 0
+        });
+    }
+    showChoices(data) {
+        if (data.options) {
+            var choices = data.options.map(data => {
+                return (
+                    <Choice
+                        key={data.label}
+                        details={data}
+                        changeAdventure={this.changeAdventure}
+                    />
+                );
+            });
+
+            return choices;
+        } else if (data.ending) {
+            return <Ending key={data.ending.code} ending={data.ending} />;
+        }
+    }
     render() {
-        return <div>Something</div>;
+        var data = this.state.adventures[this.state.current_adventure];
+        var text = data.text.split("\n").map(function(el) {
+            return <p key={el}>{el}</p>;
+        });
+
+        return (
+            <div
+                className="main-container"
+                style={{ backgroundImage: "url(" + data.image + ")" }}
+            >
+                <nav>
+                    <h1 onClick={this.resetGame}>
+                        <span className="title">This is</span> Home
+                    </h1>
+                </nav>
+                <div className="current-adventure">
+                    {text}
+                    <div className="choices">{this.showChoices(data)}</div>
+                </div>
+            </div>
+        );
     }
 }
